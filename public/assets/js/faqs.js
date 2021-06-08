e68e31acef0bcc8c9bea526c42b1734e4ae5c631
sebas favaron <sfavaron@balanz.com>
2021-06-08 12:27:14 -0300

    ADD loading faqs from API
    + fix UI
    + cleanup code
    + fix header blocking clicks

--- public/assets/js/faqs.js
+++ public/assets/js/faqs.js
@@ -1,5 +1,4 @@
 import $ from 'jquery'
-import faqs from '../json/faqs.json'
 
 // This function takes the formatted HTML
 // and inserts it into the document as
@@ -28,8 +27,6 @@ function insertAccordion(parentId, section) {
   insertHTML(parentId, html);
 
   // insert items with the data from 'faqs' json object
-  // TODO: insert items from faqs into accordionId
-  // console.log(faqs);
   for(var qaKey in section.questions) {
     const qa = section.questions[qaKey];
     insertAccordionItem(accordionId, qaKey, qa.question, qa.answer);
@@ -37,21 +34,7 @@ function insertAccordion(parentId, section) {
 }
 
 function safeString(str) {
-  // TODO: convert links to html links
-  const urlRegex = /((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w-#@]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?/
-  // const urlRegex1 = new RegExp(
-  //   "\[([^\]]*)\]\("+
-  //   urlRegex.source+
-  //   "\)"
-  // , 'g');
-  const urlRegex2 = new RegExp(
-    "\("+
-    urlRegex.source+
-    "\)"
-  , 'g');
-  // str = str.replace(urlRegex2, '<a href="$1">$1</a>');
   str = str.replace(/\[([^\]\"]*)\]\(([^\)\"]*)\)/g, '<a href="$2">$1</a>');
-  // str = str.replace(urlRegex1, '<a href="$2">$1</a>');
   
   // convert \n to <br>
   str = str.replace(/\n/g, '<br>');
@@ -75,13 +58,6 @@ function insertAccordionItem(parentId, itemId, title, content) {
         <div class="accordion-body">\
           <p>',
             safeString(content),  
-            // Welcome! We've been waiting for you. In fact, we have prepared some articles for you to get an idea on what POAP is:
-            // <br>
-            // The core value proposition of POAP:
-            // <a href="https://medium.com/@poap/the-core-value-proposition-of-poap-explained-dc379aca332d">https://medium.com/@poap/the-core-value-proposition-of-poap-explained-dc379aca332d</a>
-            // <br>
-            // What is POAP and how do I use it:
-            // <a href="https://tomso11.medium.com/what-is-poap-and-how-do-i-use-it-21ba692b918">https://tomso11.medium.com/what-is-poap-and-how-do-i-use-it-21ba692b918</a>
           '</p>\
         </div>\
       </div>\
@@ -90,14 +66,27 @@ function insertAccordionItem(parentId, itemId, title, content) {
   insertHTML(parentId, html);
 }
 
+async function getFaqs() {
+  const faqs = await fetch('https://api.poap.xyz/faq');
+  return faqs;
+}
+
 (function ($, talonUtil) {
-  const parentId = 'accordions-container';
-  const sections = faqs.sections;
-  for(var key in sections) {
-    const section = sections[key];
-    insertAccordion(parentId, section);
-  }
-  // insertAccordion("general-accordion", faqs);
-  // insertAccordion("collectors-accordion", faqs);
-  // insertAccordion("issuers-accordion", faqs);
+  // const faqs = getFaqs();
+  fetch('https://api.poap.xyz/faq')
+    .then(data => data.json()
+    .then(faqs => {
+        const parentId = 'accordions-container';
+        const sections = faqs.sections;
+        for(var key in sections) {
+          const section = sections[key];
+          insertAccordion(parentId, section);
+        }
+      })
+    )
+    .catch(function(error) {
+      console.error(error);
+    });
+
+  
 })($, window.talonUtil);
