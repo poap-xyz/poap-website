4b1622c2596b81c0c14ddc1da5ae80b2cd194cb1
Ramiro Gonzalez <ramiro@xivis.com>
2021-06-24 11:01:46 -0300

    Add new public folder to postbuild

--- postbuild.js
+++ postbuild.js
@@ -1,23 +1,32 @@
 const fs = require('fs');
 const fse = require('fs-extra');
 
-const srcDir = `public/events`;
-const destDir = `dist/events`;
-// To copy a folder or file  
-fse.copy(srcDir, destDir, function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Copied ${srcDir} to ${destDir}`);
-  }
-});
+// Add any folder from the `public` folder to be copied
+const publicFolders = ['events', '.well-known',];
+for (let i = 0; i < publicFolders.length; i++) {
+  const srcDir = `public/${publicFolders[i]}`;
+  const destDir = `dist/${publicFolders[i]}`;
+  // To copy a folder or file
+  fse.copy(srcDir, destDir, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Copied ${srcDir} to ${destDir}`);
+    }
+  });
+}
 
-const srcFile = `public/_redirects`;
-const destFile = `dist/_redirects`;
-fs.copyFile(srcFile, destFile, function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Copied ${srcDir} to ${destDir}`);
-  }
-});
+// Add any file from the `public` folder to be copied
+const publicFiles = ['_redirects',];
+for (let i = 0; i < publicFiles.length; i++) {
+  const srcFile = `public/${publicFiles[i]}`;
+  const destFile = `dist/${publicFiles[i]}`;
+  fs.copyFile(srcFile, destFile, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Copied ${srcFile} to ${destFile}`);
+    }
+  });
+}
+
