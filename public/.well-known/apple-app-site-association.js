3566dad86f7250150cfa5c83648be2ec241fe61f
Ramiro Gonzalez <ramiro@xivis.com>
2021-06-24 10:49:52 -0300

    Add deeplink files

---
+++ public/.well-known/apple-app-site-association.js
@@ -0,0 +1,15 @@
+{
+    "applinks": {
+        "details": [
+            {
+                "appIDs": [
+                    "BNJH2R62BZ.xyz.poap.app.mobile"
+                ],
+                "components": {
+                    "/": "/claim/*",
+                    "comment": "Matches any URL that starts with /claim/"
+                }
+            }
+        ]
+    }
+}
